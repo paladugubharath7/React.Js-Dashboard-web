6055c883c49d337b76b3e12eaa5b61e15b1514cb
paladugubharath7 <paladugubharath7@gmail.com>
2026-02-01 15:48:58 +0530

    alert API and mounted

--- ireporter/src/App.js
+++ ireporter/src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-export const App = () => {
+export const APP = () => {
+  const [name, setName] = useState("test")
+  //mounting
+  useEffect(() => {
+    alert("api called")
+    // api logic
+  }, [name]) // dependency array
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="rootDiv">
+      <h1>Mounted / Render App</h1>
+      <button onClick={setName('clicked')}>Click</button>
     </div>
   );
 };
-
-export default App;
